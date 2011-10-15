2620464ec929965962dc11b6296d942f233d8989
kloplop321 <kloplop321@gmail.com>
2011-10-15 15:01:40 -0500

    added a means to keep track of current items to avoid duplication or memory leak

--- include/archive.h
+++ include/archive.h
@@ -2,12 +2,14 @@
 #define ARCHIVE_H
 #include <map>
 #include <string>
+#include <set>
 #include "resource.h"
 namespace cordite {
   namespace io {
     class Archive{
     protected:
       std::map<std::string, Resource*> contents;
+      std::set<std::string> fnames;
     public:
       //Resource* getResource(const std::string fname) const;
       Resource* operator[] (const std::string fname);
