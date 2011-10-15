455a2ff9026ada3de1c6c8845a767448108dc584
kloplop321 <kloplop321@gmail.com>
2011-10-15 16:11:45 -0500

    Locator is now implemented

--- locator.cpp
+++ locator.cpp
@@ -1,7 +1,58 @@
 #include "include/locator.h"
-
+#include "include/resource.h"
+#include "include/archive.h"
 namespace cordite {
   namespace io {
-    
+    Locator::Locator(){
+      
+    }
+    Locator::~Locator(){
+      std::list<archiveAndPriority>::iterator it;
+      for(it = archives.begin(); it != archives.end(); it++){
+	archiveAndPriority& ap = (*it);
+	if(ap.arch)
+	  delete ap.arch;
+      }
+      archives.clear();
+    }
+    Resource* Locator::operator[] (const std::string fname){
+      std::list<archiveAndPriority>::iterator it;
+      for(it = archives.begin(); it != archives.end(); it++){
+	archiveAndPriority& ap = (*it);
+	if(ap.arch){
+	  Archive& arch = *ap.arch;
+	  Resource* rsc = arch[fname];
+	  if(rsc){
+	    return rsc;
+	  }
+	}
+      }
+      return 0;
+    }
+    Resource* Locator::withinPriority(
+      const std::string fname,
+      const int min,
+      const int max){
+      std::list<archiveAndPriority>::iterator it;
+      for(it = archives.begin(); it != archives.end(); it++){
+	archiveAndPriority& ap = (*it);
+	if(ap.arch && (ap.priority >= min && ap.priority <= max)){
+	  Archive& arch = *ap.arch;
+	  Resource* rsc = arch[fname];
+	  if(rsc){
+	    return rsc;
+	  }
+	}
+      }
+      return 0;
+    }
+    void Locator::addArchive(Archive* arch, const int priority){
+      archiveAndPriority a;
+      a.arch = arch;
+      a.priority = priority;
+      archives.push_front(a);
+      archives.sort();
+      archives.reverse();//So higher priority is at the beginning
+    }
   };
 };
